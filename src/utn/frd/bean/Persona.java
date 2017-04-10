26787977d83c12a66b9d9b8036ef05f39cb20549
faustosaccone <fausto_2-8@hotmail.com>
2017-04-09 23:44:24 -0300

    terminado

--- src/utn/frd/bean/Persona.java
+++ src/utn/frd/bean/Persona.java
@@ -1,22 +1,49 @@
 package utn.frd.bean;
 
 public class Persona {
+	
 	private long id;
 	private String name;
 	private int age;
 	private String gender;
-	private int casa;
 	
-	public Persona(int identificador, String nombre, int edad, String genero){
-		id = identificador;
-		name = nombre;
-		age = edad;
-		gender = genero;
-	}
-	
-	public static void main(String[] args) {
-		
+	public Persona(int id, String name, int edad, String gender) {
+		this.id = id;
+		this.name = name;
+		this.age = edad;
+		this.gender = gender;
 	}
 
+	public int getAge() {
+		return age;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
 }
 
